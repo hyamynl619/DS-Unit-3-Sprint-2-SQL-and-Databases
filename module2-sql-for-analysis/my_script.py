f6ccc3a461331d82e19cabd8e7e10f089b2ed4a6
hyamynl619 <amy.nguyenle619@gmail.com>
2020-02-18 14:07:04 -0500

    ..

--- module2-sql-for-analysis/my_script.py
+++ module2-sql-for-analysis/my_script.py
@@ -1,20 +1,22 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
 
-import pyscopg2
+load_dotenv()  # > loads contents of the .env file into the script's environment
 
-print("CONNECTING TO THE DATABASE...")
+DB_NAME = 'dtbvkzle'
+DB_USER = 'dtbvkzle'
+DB_PASSWORD = 'M6VvGo-LtzJjqh6oqErwJmwQ7VM53doZ'
+DB_HOST = 'rajje.db.elephantsql.com'
 
-conn = psycopg2.connect(
-    dbname = 'dtbvkzle',
-    user= 'dtbvkzle',
-    password='M6VvGo-LtzJjqh6oqErwJmwQ7VM53doZ',
-    host= 'rajje.db.elephantsql.com'
-)
+connection = psycopg2.connect(
+    dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 print("CONNECTION:", connection)
 
 cursor = connection.cursor()
 print("CURSOR:", cursor)
+cursor.execute('SELECT * from test_table;')
 
-cursor.execute('SELECT * test_table;')
 result = cursor.fetchall()
 print("RESULT:", type(result))
 print(result)
